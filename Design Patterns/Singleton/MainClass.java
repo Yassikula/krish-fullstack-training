db18e0c1aabd90a22b6147e270920091f2674899
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-07-17 15:46:31 +0530

    Update MainClass.java

--- Design Patterns/Singleton/MainClass.java
+++ Design Patterns/Singleton/MainClass.java
@@ -1,1 +1,16 @@
+package Singleton;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+    	
+    	singletonClass s1 = singletonClass.getName();
+    	System.out.println(s1);
+    	
+    	
+    	singletonClass s2 = singletonClass.getName();
+    	System.out.println(s2);
+
+    }
+}
 
