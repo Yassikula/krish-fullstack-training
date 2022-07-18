d76c11e0b16fde25fcef9596ceb0a8d2b8f66028
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-07-18 07:18:49 +0530

    chain of responsibility added

---
+++ Design Patterns/ChainOfRes/BTT.java
@@ -0,0 +1,18 @@
+package ChainOfRes;
+
+public class BTT extends Handler{
+	
+	 @Override
+	    public double applyTax(Invoice invoice) {
+	        invoice.setTax(invoice.getTax() + invoice.getAmount() * 0.03);
+	        System.out.println("BTT Calculated");
+
+	        if (invoice.getAmount() > 200.0 && invoice.getAmount() <= 300.0) {
+	            return invoice.getTax();
+	        } else {
+	         
+	            return successor.applyTax(invoice);
+	        }
+	    }
+
+}
