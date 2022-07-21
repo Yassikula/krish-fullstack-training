ff6caebf262c1870065c43230d540c5b8cabc288
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-07-21 07:30:46 +0530

    files added on microservices

---
+++ Rentacar -Microservices/video 5/profile-service/src/main/java/com/yasasi/rentcloud/profileservice/service/CustomerService.java
@@ -0,0 +1,8 @@
+package com.yasasi.rentcloud.profileservice.service;
+
+import com.yasasi.rentcloud.commons.model.Customer;
+
+public interface CustomerService {
+
+    Customer save(Customer customer);
+}
