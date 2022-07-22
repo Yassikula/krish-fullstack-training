95ee65fc965a3f229d306848d4aa41b15258599e
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-07-22 12:00:15 +0530

    Delete Spring-Fundementals/SpringJavaLearning directory

--- Spring-Fundementals/SpringJavaLearning/src/com/krishantha/training/salesmanager/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.krishantha.training.salesmanager.service;
-
-import com.krishantha.training.salesmanager.model.Employee;
-
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> getAllEmployees();
-}
