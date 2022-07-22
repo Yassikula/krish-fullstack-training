cbda9a0ac68d1c27565c9a3049e909493ab623ec
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-07-22 15:42:01 +0530

    added video 15

--- Rentacar -Microservices/video 15/vehicle-service/src/main/java/lk/codelabs/rentcloud/vehicleservice/VehicleServiceApplication.java
+++ Rentacar -Microservices/video 15/vehicle-service/src/main/java/lk/codelabs/rentcloud/vehicleservice/VehicleServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EntityScan(basePackages = "lk.codelabs.rentcloud.model.vehicle")
 public class VehicleServiceApplication {
 
 	public static void main(String[] args) {
