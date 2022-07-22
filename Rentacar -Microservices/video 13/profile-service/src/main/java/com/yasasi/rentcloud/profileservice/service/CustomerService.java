3c125c2441d13606cb61cc5853b6d09248012f4d
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-07-22 05:53:10 +0530

    add changes

---
+++ Rentacar -Microservices/video 13/profile-service/src/main/java/com/yasasi/rentcloud/profileservice/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.yasasi.rentcloud.profileservice.service;
+
+import com.yasasi.rentcloud.commons.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+
+    Customer save(Customer customer);
+
+    Customer fetchById(int profileId);
+
+    List<Customer> fetchAllProfiles();
+}
