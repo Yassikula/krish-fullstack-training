a1b5753e2b2b48f53c2ab91bbff636377907d34c
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-08-12 06:17:40 +0530

    update code

--- Fuel-Application/FuelApplication/src/app/order.service.ts
+++ Fuel-Application/FuelApplication/src/app/order.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from './order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+ 
 
  
   constructor(private http: HttpClient) { }
 
-  API = 'http://localhost:9090';
+  API = 'http://localhost:9090/backend/orders';
   public orderSubmit(orderData :any){
     return this.http.post(this.API + '/orderSubmit' , orderData);
 }
@@ -18,15 +21,20 @@ public getAllOrders(){
   return this.http.get(this.API + '/getAllorders');
 
 }
-public OrderReceived(stationId : any){
+public OrderReceived(id : number, status : string){
 
-  return this.http.get(this.API + '/receiveOrder/{stationId}', stationId);
+  return this.http.put(`${this.API}/receiveOrder/${id}/${status}`, "");
 }
 
-public getOrderById(stationId : any){
+public getOrderById(id : number): Observable<Order[]>{
 
-  return this.http.get(this.API + 'getorder/{stationId}', stationId);
+
+  return this.http.get<Order[]>(`${this.API}/getorder/${id}`);
 
 }
+getOrderComplete(id: number, status: string) {
+  return this.http.put(`${this.API}/receiveOrder/${id}/${status}`,"");
+}
+
 
 }
