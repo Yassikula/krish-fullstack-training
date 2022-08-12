d05a786ec257d15b37a6452a34853152d1dc190e
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-08-12 06:07:14 +0530

    edit service class

--- Fuel-Application/dispatchApp/src/main/java/yasasi/app/dispatchApp/service/ScheduleService.java
+++ Fuel-Application/dispatchApp/src/main/java/yasasi/app/dispatchApp/service/ScheduleService.java
@@ -3,12 +3,15 @@ package yasasi.app.dispatchApp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import yasasi.app.dispatchApp.config.DispatcTopic;
+import yasasi.app.dispatchApp.config.ScheduleTopic;
 import yasasi.app.dispatchApp.model.ScheduleFuel;
 import yasasi.app.dispatchApp.repository.SchRepository;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+
 
 @Service
 public class ScheduleService {
@@ -17,26 +20,27 @@ public class ScheduleService {
     @Autowired
     SchRepository schRepository;
 
-    public ScheduleFuel scheduleDate(int orderId, String fuelType, int capacity) {
+    @Autowired
+    KafkaTemplate<String,ScheduleFuel> kafkaTemplate;
+
+    public ScheduleFuel scheduleDate(int orderId ) {
 
 
 
         ScheduleFuel schedule = new ScheduleFuel();
         schedule.setOrderId(orderId);
-        schedule.setFuelType(fuelType);
-        schedule.setQty(capacity);
-        schedule.setScheduleDate();
-        schedule.setStatus("Scheduled");
+        schedule.setScheduleDate(LocalDate.now().plusDays(2).toString());
         saveSchedule(schedule);
         return schedule;
 
     }
 
-    public ResponseEntity<ScheduleFuel> saveSchedule(ScheduleFuel schedule) {
+    public ScheduleFuel saveSchedule(ScheduleFuel schedule) {
 
-        schRepository.save(schedule);
+        kafkaTemplate.send(ScheduleTopic.MESSAGE_TOPIC,schedule);
+        kafkaTemplate.send(DispatcTopic.MESSAGE_TOPIC,schedule);
+           return schRepository.save(schedule);
 
-        return ResponseEntity.status(HttpStatus.OK).body(schedule);
     }
 
 }
