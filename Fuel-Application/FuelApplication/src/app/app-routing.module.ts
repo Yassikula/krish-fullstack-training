e29ac17364478efc5ff70669fff60c492e0085c5
Yasasi Kulathunge <47667953+Yassikula@users.noreply.github.com>
2022-08-12 06:22:19 +0530

    update code

--- Fuel-Application/FuelApplication/src/app/app-routing.module.ts
+++ Fuel-Application/FuelApplication/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { DispatchOrederComponent } from './dispatch-order/dispatch-order.component';
 const routes: Routes = [
 
-  
+  {path: 'dispatch-order', component: DispatchOrederComponent},
 ];
 
 @NgModule({
